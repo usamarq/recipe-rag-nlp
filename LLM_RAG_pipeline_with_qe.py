5d89ffb2602d83cdd4c82fc8746bd25680fa618f
Abdullah Hassan <abdullah.hassan@talentadore.com>
2025-11-02 17:34:32 +0100

    updated path name

--- LLM_RAG_pipeline_with_qe.py
+++ LLM_RAG_pipeline_with_qe.py
@@ -44,7 +44,7 @@ class RecipeRAGSystem:
     def __init__(self, csv_path, 
                  embedding_model="nomic-embed-text:latest", 
                  llm_model="llama3.1:latest", 
-                 vector_store_path="faiss_recipe_index",
+                 vector_store_path="faiss_recipe_index_nomic",
                  force_rebuild=False, 
                  batch_size=50):
         """
